2db69492939f12fc074fe230add5ad77a8f6b30a
Toph Bantoto <lcbantoto@gmail.com>
2022-04-28 03:25:41 -0400

    fix footer sizing/spacing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const footerLeft: React.CSSProperties = {
+    flex: 0.1,
+  }
   const footerRight: React.CSSProperties = {
+    flex: 0.9,
+    minWidth: "120px",
     justifyContent: 'end',
+    fontSize: "xx-small",
+  }
+  const pLeft: React.CSSProperties = {
+    minWidth: "88px"
   }
 
   return (
     <div>
       <Component {...pageProps} />
       <footer className={"Home_footer____T7K"}>
-        <div>
-          <p>Built with <svg height={"0.8rem"} viewBox="0 0 1792 1792" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg"><path d="M896 1664q-26 0-44-18l-624-602q-10-8-27.5-26T145 952.5 77 855 23.5 734 0 596q0-220 127-344t351-124q62 0 126.5 21.5t120 58T820 276t76 68q36-36 76-68t95.5-68.5 120-58T1314 128q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z" fill="#e25555"></path></svg> by </p>
+        <div style={footerLeft}>
+          <p style={pLeft}>Built with <svg height={"0.8rem"} viewBox="0 0 1792 1792" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg"><path d="M896 1664q-26 0-44-18l-624-602q-10-8-27.5-26T145 952.5 77 855 23.5 734 0 596q0-220 127-344t351-124q62 0 126.5 21.5t120 58T820 276t76 68q36-36 76-68t95.5-68.5 120-58T1314 128q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z" fill="#e25555"></path></svg> by </p>
           <a href="https://twitter.com/Kuyachi_" target="_blank" rel="noreferrer"><p>Kuyachi</p></a>
         </div>
         <div style={footerRight}>
